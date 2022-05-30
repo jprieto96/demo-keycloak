4f900d81cc70b2d1a0bd11a6b05a208219e1f264
Jose Ramon Prieto del Prado <jose.delprieto@stratesys-ts.com>
2022-05-30 12:25:58 +0200

    added new env variables

--- src/main/java/com/example/demo/config/KeycloakConfiguration.java
+++ src/main/java/com/example/demo/config/KeycloakConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeycloakConfiguration {
-
     @Bean
     public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
